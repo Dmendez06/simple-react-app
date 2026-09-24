4f363bb2fa8af403d55f1476a2b945a5e440e28e
Dylan <dylanmendezzamora@gmail.com>
2026-09-24 15:16:46 -0600

    arreglo del set data

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   useEffect(() => {
     const loadProducts = async () => {
       const data = await fetchProducts();
-      setProducts(data.content);
+      setProducts(data?.content || data || []);
       setLoading(false);
     };
     loadProducts();
